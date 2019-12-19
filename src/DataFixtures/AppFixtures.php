b9fae786fa1666a0af9cd1f6872c99c9b1cb3aa0
mchandelon <mchandelon@iparla.iutbayonne.univ-pau.fr>
2019-12-19 21:32:10 +0100

    Création d'un module de test en BD et ajout de composant fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,14 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
+use App\Entity\Formations;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $formationDUTInfo = new Formations();
+		$formationDUTInfo->setNom("DUT Informatique");
+        $manager->persist($formationDUTInfo);
 
         $manager->flush();
     }
